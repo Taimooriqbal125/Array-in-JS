c677ab1467971b76b070bbb50dd46c2ab0879db5
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-06-03 00:00:34 -0700

    Array in JS

---
+++ script.js
@@ -0,0 +1,14 @@
+//  Array are consective date space.
+// start form 0 to onwards.
+
+var temii = ["ali", "amjad" ,"hussain" , "amna" ,"adil" ,"shery"];
+//  in this temii is array name and data in [] is data of array
+
+console.log("Original" , temii);
+// output whole array data
+
+console.log("value in 3 index" , temii[3]);
+// output value in 3rd index
+
+//  FIND LENGTH OF ARRAY 
+console.log(temii.length);
