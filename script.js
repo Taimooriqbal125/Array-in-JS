50d67225eeafb0e11ca0647b2c8bc4fbe2b536fb
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-06-04 02:16:12 -0700

    Array

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 //  Array are consective date space.
 // start form 0 to onwards.
 
-var temii = ["ali", "amjad" ,"hussain" , "amna" ,"adil" ,"shery"];
+// var temii = ["ali", "amjad" ,"hussain" , "amna" ,"adil" ,"shery"];
 //  in this temii is array name and data in [] is data of array
 
-console.log("Original" , temii);
+// console.log("Original" , temii);
 // output whole array data
 
-console.log("value in 3 index" , temii[3]);
+// console.log("value in 3 index" , temii[3]);
 // output value in 3rd index
 
 //  FIND LENGTH OF ARRAY 
-console.log(temii.length);
+// console.log(temii.length);
+
+
+
+var nameofstudents = ["ali", "amjad", "hussaon", "kamran","battol","rajag","puta","mariam","zinad","hjii","malik","fraedd","raja"];
+
+ 
+nameofstudents.push("jasmin","sultam","bhaii");
+console.log("LENGTH OF ORIGINAL" , nameofstudents.length);
+console.log("ORIGINAL" , nameofstudents);
+
+
